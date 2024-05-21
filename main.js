75c04c2c26a97a359d1654a96e233c7bcc9e791b
akom_work <akom07@gmail.com>
2024-05-21 18:29:12 +0300

    done

---
+++ main.js
@@ -0,0 +1,42 @@
+let rect1 =6
+let rect2 =7
+let area =rect1*rect2
+console.log(`
+The area of the rectangle is: ${area} m2
+`);
+//-----------------------
+let C=25
+let F= (9/5)*C+32
+console.log(`${F} F° is ${C} C°
+`);
+//-----------------------
+hour = 2
+seconds = hour * 60 * 60
+console.log(`${hour} hours = ${seconds} Seconds
+`);
+//-----------------------
+firstName = "amer ";
+lastName = "alghamdi";
+fullName = firstName.concat(lastName)
+console.log(fullName + `
+` );
+//-----------------------
+let bill = 10.25 + 3.99 + 7.15
+let tip = bill * 0.15
+let total = bill + tip
+console.log(total +`
+`);
+//-----------------------
+let adj1 = "amazing";
+let adj2 = "fun";
+let adj3 = "entertaining";
+let madLib = `The Intro to JavaScript course is ${adj1}. James and Julia are so ${adj2}. I cannot wait to work through the rest of this ${adj2} content! 
+`
+console.log(madLib);
+//-----------------------
+let FName = "amer"
+let interest = "programming"
+let hobby = "JavaScript"
+let awesomeMessage = `Hi, my name is ${FName}. I love ${interest}. In my spare time, I like to ${hobby}.
+`
+console.log(awesomeMessage);
